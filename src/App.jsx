1e598230ba578b4d3ca27c2374f017c6dfaf4c93
sahoomonalisa123 <monalisasahoo930@gmail.com>
2025-01-21 21:35:19 +0530

    Ecommerce website

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import Aos from "aos"
+import "aos/dist/aos.css";
+import { useEffect, useState } from "react";
+import Banner from "./Components/Banner/Banner";
+import Navbar from "./Components/Navbar/Navbar";
+
+function App() {
+     const [ orderPopup, setOrderPopup] = useState(false);
+
+     const handleOrderPopup = () => {
+      setOrderPopup(!orderPopup);
+     };
+
+
+     useEffect(() => {
+      Aos.init({
+        offset: 100,
+        duration: 800,
+        easing: "ease-in-sine",
+        delay: 100,
+      });
+      Aos.refresh();
+     }, []);
+
+  return (
+    <>
+      <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
+        <Navbar handleOrderPopup={handleOrderPopup}/>
+      <Banner/>
+      </div>
+    
+    </>
+  )
+}
+
+export default App
