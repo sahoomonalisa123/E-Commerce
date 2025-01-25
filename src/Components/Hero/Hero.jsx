1051e2d3f7498718fa10acf6dff4abaf4536ef23
sahoomonalisa123 <monalisasahoo930@gmail.com>
2025-01-25 22:15:59 +0530

    Add Hero section

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Image1 from "../../assets/women.png";
+import Image2 from "../../assets/hero/shopping.png";
+import Image2 from "../../assets/hero/sale.png";
+
+import Slider from "react-slick";
+
+const ImageList = [
+    {
+    id: 1,
+    img: Image1,
+    title: "Upto 50% off on all Men's Wear ",
+    description: 
+    "iure voluptates quos est maiores illo quis officia odit minima cum itaque illum quod nisi alias minus aperiam voluptate asperiores ratione neque
+     in repellat dolores maxime fugit! Quo natus deleniti id neque accusantium laudantium, minima voluptates, dolore molestias numquam quaerat adipisci voluptatibus maiores impedit iste quam saepe.
+      Amet atque sed voluptates laborum incidunt, facere praesentium dolorum at perspiciatis possimus expedita, modi cupiditate neque temporibus quo porro qui ipsum maxime accusamus molestiae soluta 
+      tenetur recusandae.  illo accusamus dignissimos nesciunt nostrum, temporibus animi minima repudiandae numquam.
+     Odio eligendi est nesciunt q"
+    },
+]
+
+
+
+
+const Hero = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Hero
